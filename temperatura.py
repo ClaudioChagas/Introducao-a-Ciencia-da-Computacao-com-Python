251b5879750385de10f0f72c8809190fb61c0c9f
Claudio Augusto de Souza Chagas <88558044+ClaudioChagas@users.noreply.github.com>
2021-10-07 16:27:34 -0300

    Add files via upload

---
+++ temperatura.py
@@ -0,0 +1,11 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+>>> temperaturafahrenheit= 78
+>>> 
+>>> temperaturaCelsius= (temperaturafahrenheit - 32)* 5 / 9
+>>> 
+>>> print(" a temperatura em celsius é", temperaturaCelsius)
+ a temperatura em celsius é 25.555555555555557
+>>> 
